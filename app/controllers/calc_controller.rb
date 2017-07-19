57bf871661f9f5ab269eebb3a61d7cb10b36876a
kazu <g031n085@s.iwate-pu.ac.jp>
2017-07-19 03:33:51 +0000

    four arithmetic operation

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -1,13 +1,27 @@
 class CalcController < ApplicationController
+  
+  before_action :before_action
+  
   def addition
+    @ans = @num1 + @num2
   end
 
   def substraction
+    @ans = @num1 - @num2
   end
 
   def multiplication
+    @ans = @num1 * @num2
   end
 
   def division
+    @ans = @num1 / @num2
+  end
+  
+  private
+  
+  def before_action
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
   end
 end
