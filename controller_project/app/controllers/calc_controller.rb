491eaa77c9ef1c04cb2705832fc48a0ea7f270ea
kazu <g031n085@s.iwate-pu.ac.jp>
2017-07-19 03:08:58 +0000

    create calc controller

---
+++ controller_project/app/controllers/calc_controller.rb
@@ -0,0 +1,16 @@
+class CalcController < ApplicationController
+  
+  before_action
+  
+  def addition
+  end
+
+  def substraction
+  end
+
+  def multiplication
+  end
+
+  def division
+  end
+end
--- controller_project/config/routes.rb
+++ controller_project/config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  get 'calc/addition/:num1/:num2' => 'calc#addition'
+
+  get 'calc/substraction/:num1/:num2' => 'calc#substraction'
+
+  get 'calc/multiplication/:num1/:num2' => 'calc#multiplication'
+
+  get 'calc/division/:num1/:num2' => 'calc#division'
+
+  get 'greeting/morning'
+
+  get 'greeting/afternoon'
+
+  get 'greeting/evening'
+  
+  get 'greeting/echo/:echo' => 'greeting#echo'
+  
+  get 'greeting/random' => 'greeting#random'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'books/hello' => 'books#hello'
+  
+  get 'books/:id' => 'books#show'
 end
